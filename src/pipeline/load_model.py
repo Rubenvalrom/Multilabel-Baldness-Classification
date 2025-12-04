1c2ff0fd7254f6cf15ea2ebf9ce867ebc8305328
Rubenvalrom <ruben627vr@gmail.com>
2025-12-04 17:07:32 +0100

    Update .gitignore and set model version to 1
    Expanded .gitignore to include .pyc, .pyo, and recursive __pycache__ patterns. Changed the model_version in load_model.py from 2 to 1 to load the correct model version.

--- src/pipeline/load_model.py
+++ src/pipeline/load_model.py
@@ -7,7 +7,7 @@ def load_model(device="cpu"):
     model_name = "Alopecia Classifier"
 
     # Versión específica
-    model_version = 2
+    model_version = 1
 
     # Cargar el modelo desde el registro
     model = mlflow.pytorch.load_model(model_uri=f"models:/{model_name}/{model_version}")
